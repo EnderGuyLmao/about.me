62451965ef8c76a88695a38de5502abe00547855
Ender <guyender866@gmail.com>
2025-09-28 15:57:42 +0200

    overall about me rework

---
+++ music_manager.js
@@ -0,0 +1,63 @@
+const music = document.getElementById("MUSIC");
+music.muted = true;
+music.volume = 0.5;
+const main_container = document.getElementById("MAIN");
+let rot = 0;
+const rotating_cube_1 = document.getElementById("RotatingBGCube1");
+const rotating_cube_1_1 = document.getElementById("RotatingBGCube1-1");
+const music_toggler = document.getElementById("MusicToggler");
+const music_thumb = document.getElementById("MusicThumb");
+
+let ctx, src, analyzer;
+
+music_toggler.addEventListener("click", async () => {
+    music.muted = !music.muted;
+    MusicThumb.classList.toggle("active");
+
+    if (music.paused) {
+        await music.play().catch(err => console.log("Playback failed:", err));
+    }
+
+    if (!ctx) {
+        ctx = new AudioContext();
+        src = ctx.createMediaElementSource(music);
+        analyzer = ctx.createAnalyser();
+
+        src.connect(analyzer);
+        analyzer.connect(ctx.destination);
+
+        analyzer.fftSize = 256;
+    }
+
+    if (ctx.state === "suspended") {
+        await ctx.resume();
+    }
+});
+
+const dataArray = new Uint8Array(128);
+
+function lerp(a, b, t) {
+    return a + (b - a) * t;
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (analyzer) {
+        analyzer.getByteFrequencyData(dataArray);
+
+        let sum = 0;
+        for (let i = 0; i < dataArray.length; i++) sum += dataArray[i];
+        let avg = sum / dataArray.length;
+
+        let t = avg / 255;
+        
+        rotating_cube_1.style.opacity = t;
+
+        rot = lerp(rot, rot + (avg/10), t);
+        rotating_cube_1.style.transform = `translate(-50%, -50%) rotate(${rot}deg)`
+        rotating_cube_1_1.style.transform = `translate(50%, 50%) rotate(-${rot/2}deg)`
+    }
+}
+
+animate();
